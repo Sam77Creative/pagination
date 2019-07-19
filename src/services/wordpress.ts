8fb6ab4b8292872a2c6426759308a1f6902b16de
Sam Gallagher <sam@77creative.io>
2019-07-19 11:50:38 -0500

    updated pagOpts.wordpress

--- src/services/wordpress.ts
+++ src/services/wordpress.ts
@@ -1,14 +1,10 @@
 import { IPaginationOptions } from "../interfaces/core.interfaces";
 
 export const WordpressOpts: IPaginationOptions = {
-  totalRecords: {
-    header: "x-wp-total"
-  },
-  totalPages: {
-    header: "x-wp-totalpages"
-  },
   page: {
-    query: "page"
+    headerLink: {
+      header: "link"
+    }
   },
   recordsPerPage: 10,
   type: "JSON"
